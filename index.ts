5e6fa579132cf8e69ca2ff231f604b9bf81f3d52
AnthocDev <anthocdev@gmail.com>
2021-03-09 13:01:32 +0000

    port changes

--- index.ts
+++ index.ts
@@ -5,15 +5,12 @@ const app = express();
 import * as ws from "ws";
 import WebSocketServer from "./websocket";
 
-
-
-
 const _server = http.createServer(app);
-WebSocketServer.init(_server)
+WebSocketServer.init(_server);
 // this._server.on("upgrade", function upgrade(request, socket, head){
-    
+
 // })
 
-_server.listen(3000, () => {
-    console.log(`Server UP on port ${_server.address()}`)
-})
+_server.listen(process.env.PORT || 3000, () => {
+  console.log(`Server UP on port ${_server.address()}`);
+});
